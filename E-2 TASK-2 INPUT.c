48bc335830fac2265481c503ce8357a005444549
ajaybabuchavan-cmd <ajaybabuchavan@eng.rizvi.edu.in>
2025-10-17 13:43:33 +0530

    Add files via upload

---
+++ E-2 TASK-2 INPUT.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, sum = 0, i;
+
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    for(i = num1 + 1; i < num2; i++) {
+        if(i % 2 != 0) {
+            sum = sum + i;
+        }
+    }
+
+    printf("Sum of odd numbers between %d and %d is %d\n", num1, num2, sum);
+    return 0;
+}
