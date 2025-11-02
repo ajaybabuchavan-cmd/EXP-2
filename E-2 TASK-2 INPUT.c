75c60b67432e406db9380af51df8eb69df0fe2f7
ajaybabuchavan-cmd <ajaybabuchavan@eng.rizvi.edu.in>
2025-11-02 23:31:40 +0530

    Refactor to sum odd numbers in user-defined range
    Updated the program to calculate the sum of odd numbers between a user-defined range instead of a fixed range.

--- E-2 TASK-2 INPUT.c
+++ E-2 TASK-2 INPUT.c
@@ -1,21 +1,20 @@
-//Ajay Babu Chavan,UIN:-251P081,Year:-F.E,Division:-D
+Ajay Babu Chavan,UIN:-251P081,Year:-F.E,Division:-D
 #include <stdio.h>
 
 int main() {
-    int num1, num2, sum = 0, i;
 
-    printf("Enter first number: ");
-    scanf("%d", &num1);
-    printf("Enter second number: ");
-    scanf("%d", &num2);
+    int start, end;
+    int sum = 0;
 
-    for(i = num1 + 1; i < num2; i++) {
-        if(i % 2 != 0) {
-            sum = sum + i;
+    printf("Enter the limit: ");
+    scanf("%d %d", &start, &end);
+
+    for (int i = start; i <= end; i++) {
+        if (i % 2 != 0) {
+            sum += i;
         }
     }
 
-    printf("Sum of odd numbers between %d and %d is %d\n", num1, num2, sum);
+    printf("Sum of odd numbers is %d", sum);
     return 0;
-
 }
